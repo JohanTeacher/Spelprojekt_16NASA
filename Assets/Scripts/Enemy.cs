c2d57d6250955544c3a6898d6308f1e44950b657
epiczed00 <malcom2000@live.se>
2018-11-21 09:29:06 +0100

    h
    h

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,6 +13,8 @@ public class Enemy : MonoBehaviour
 
     public GameObject bullet;   //the bullet we are shooting must have a rigidbody
     public int Speed = 32000; //the speed the bullet is shot at
+    public int speed = 30;
+    public Hiddentrigger Triggerss;
 
     //this is only used if rapid fire is set to true
     //RateOfFire privat
@@ -27,6 +29,12 @@ public class Enemy : MonoBehaviour
         GameObject shotRapid = Instantiate(bullet, transform.position - new Vector3(2, 0, 0), Quaternion.identity);
 
         shotRapid.GetComponent<Rigidbody2D>().AddForce(new Vector2(-Speed, 0));
+
+
+
+
+
+
     }
 
 
@@ -42,8 +50,50 @@ public class Enemy : MonoBehaviour
         GameObject shotRapid = Instantiate(bullet, transform.position - new Vector3(2, 0, 0), Quaternion.identity);
 
         shotRapid.GetComponent<Rigidbody2D>().AddForce(new Vector2(-Speed, 0));
+        int c = Triggerss.Ones(1);
+
+
+        if (c == 1)
+        {
+
+
+            tc = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            transform.position = Vector2.MoveTowards(transform.position, tc.position, speed * Time.deltaTime);
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
 
 
     }
 
-}
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/Hiddentrigger.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hiddentrigger : MonoBehaviour {
+
+
+    int kaka;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+
+            ;
+
+        }
+
+
+    }
+
+
+    int Ones(int x){
+
+
+        if (x == 1)
+        {
+
+
+
+
+            kaka = 1;
+
+        }
+
+
+        return kaka;
+
+
+    }
+
+
+
+}
